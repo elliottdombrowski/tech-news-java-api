2ffe6c981f9b5a3e9758eba5f11575b0177a73a9
elliott dombrowski <elliottroyal@me.com>
2022-01-25 15:14:14 -0600

    added variabletypes class

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 3.5;
+
+        double sum = a + b;
+        System.out.println(sum);
+
+        String c = "dog";
+        String sum2 = a + c;
+        System.out.println(sum2);
+    }
+}
