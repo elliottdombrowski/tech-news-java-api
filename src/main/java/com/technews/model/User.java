daa243807c54bf1a1380235476ce9d599074bbcf
elliott dombrowski <elliottroyal@me.com>
2022-01-25 15:44:46 -0600

    added user variable-level annotations

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -8,6 +8,14 @@ import javax.persistence.Table;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
 
-
 public class User {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    boolean loggedIn;
+
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
 }
